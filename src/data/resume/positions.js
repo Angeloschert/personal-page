359896935146bee32778b19586861098e4036cbb
Angelo Huang <chermissote@gmail.com>
2021-08-12 11:59:07 -0400

    changed the positions

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -4,18 +4,22 @@ const positions = [
     position: 'Software Engineer',
     daterange: 'May 2021 - Present',
     points: [
-      'Waltson Health AI',
+      'Developed the alerting system using Prometheus to monitor the cluster health and visualize the system spectrums of worker nodes on Grafana',
+      'Automated the procedures using Ansible playbooks to instantiate the subscription for Grafana operator and service accounts, and to create the dashboard UI with unique routings',
+      'Contributed to the deployment pipeline for worker node VM provisioning under from template VMs, their configurations on network/hardwares in vSphere Center and root password change in batches',
+      'Responsible for pipeline environment health checks and automated cleanups in multiple namespaces including goss checks, docker pruning and orphaned k8s volumes cleanup'
     ],
   },
   {
     company: 'Ericsson',
-    position: '4G/5G Software Development Engineer',
+    position: '5G/LTE Software Development Engineer',
     daterange: 'May 2020 - April 2021',
     points: [
-      'Implemented the commit stats manipulation workflow that stores the commit and deployment status in Elasticsearch and visualizes the stats in Kibana',
-      'Designed the MTD Counter Analyzer with Python which categorizes the MTD counters by domains, calculates rates and other stats',
-      'Developed the NR DBC token report page that reports token information from the NR dev codebase including their locations, occurences and trends with commit details. The webpage is built with React, Node.js, HTML and CSS/SASS, and fetches token data from a Jenkins instance running the backend Python logics. Yaml config files are used to define the desired tokens and the search scope.',
-      'Built the NR metrics dashboard with React that feeds the performance backlog from NR components and reports quantitaive stats, and established SQL log database to store incoming data and developed the server-side connection service with Express. The dashboard enables multi-level filtering on log query including component category, suite, and logging periods, and also tabularized the backlog stats and visualized each log type using amChart framework to show the value trends versus dates. It facilitates the signal analysis by 30% and greatly reduces the assessment workload.',
+      'Worked in the Baseband Tool Development Team under the Agile SCRUM process.',
+      'Developed the NR Metrics Dashboard that feeds the performance backlogs from NR components and reports the key token stats(commits info, SHA, source files and line numbers etc) from source codes in the NR DBC codebase',
+      'Established MongoDB database to process incoming device logs and implemented the server-side REST APIs with Express that supports CRUD operations to handle multi-level filtering on the query',
+      'Built the backend business infrastructure in Python to fetch source codes, integrate the token information into json files, compress the output json to form the code archive and generate final meta for the frontend page',
+      'Contributed to the frontend that obtains token data from Jenkins job outputs, as well as implemented the dynamic graphing system and monitoring overview page to trigger alerts for detected under-performed LTE devices',
     ],
   },
 ];
